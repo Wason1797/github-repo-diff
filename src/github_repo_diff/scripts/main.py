aa89db2fd7aa8ee2d49be5aa340c211222841114
Wladymir Brborich <wabrborich@hotmail.com>
2022-08-27 15:12:34 -0500

    Add the option to store results in a file
    * Improve argparse configurations
    
    * Add the out_file parameter to the main function

--- src/github_repo_diff/scripts/main.py
+++ src/github_repo_diff/scripts/main.py
@@ -1,7 +1,9 @@
 import asyncio
 from argparse import ArgumentParser
+from typing import Optional
 
 import httpx
+import aiofiles
 from github_repo_diff.comparator import (build_file_tree, compare_file_trees,
                                          make_row)
 from github_repo_diff.downloader import (configure_dirs, download_repo,
@@ -11,15 +13,16 @@ from prettytable import PrettyTable
 
 def get_args() -> tuple:
     parser = ArgumentParser(description='Get a comparison percentage from two GitHub Forks')
-    parser.add_argument("-la", "--left_author", help="GitHub author for the left fork", type=str)
-    parser.add_argument("-ra", "--right_author", help="GitHub author for the right fork", type=str)
-    parser.add_argument("-r", "--repo", help="Name of the GitHub repo", type=str)
-    parser.add_argument("-o", "--out", help="Output path", type=str, default='./repo_dist')
+    parser.add_argument("-la", "--left_author", help="GitHub author for the left fork", type=str, required=True)
+    parser.add_argument("-ra", "--right_author", help="GitHub author for the right fork", type=str, required=True)
+    parser.add_argument("-r", "--repo", help="Name of the GitHub repo", type=str, required=True)
+    parser.add_argument("-d", "--dist", help="Download Directory", type=str, default='./repo_dist')
+    parser.add_argument("-of", "--out_file", help="Output File", type=str)
     args = parser.parse_args()
-    return args.left_author, args.right_author, args.repo, args.out
+    return args.left_author, args.right_author, args.repo, args.dist, args.out_file
 
 
-async def main(left_author: str, rihgt_author: str, repo: str, path: str) -> None:
+async def main(left_author: str, rihgt_author: str, repo: str, path: str, out_file: Optional[str] = None) -> None:
 
     async with httpx.AsyncClient() as client:
 
@@ -39,10 +42,18 @@ async def main(left_author: str, rihgt_author: str, repo: str, path: str) -> Non
     filtered_result = {file: data for file, data in result.items() if not set(file.split('/')).intersection(rejected)}
 
     table.add_rows(make_row(file, data, repo) for file, data in filtered_result.items())
-
-    print(table)
     average_similarity = sum(data['percent'] for data in filtered_result.values())/len(filtered_result)
-    print(f'=> Average Similarity = {average_similarity:.2f}%')
+    similarity_label = f'=> Average Similarity = {average_similarity:.2f}%'
+    if not out_file:
+        print(table)
+        print(similarity_label)
+        return
+
+    async with aiofiles.open(f'./{out_file}', 'w') as output:
+        await output.write(f'{table}\n')
+        await output.write(f'{similarity_label}\n')
+
+    print(f'Results stored in {out_file}')
 
 
 def entry_point() -> None:
